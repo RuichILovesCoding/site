5d406db2fe8d71e4abd574598b42f673ffeb12ac
Ruichi Wu <wurc199733@gmail.com>
2020-07-23 15:57:40 +1000

    modal done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import "../styles/styles.css";
 import MobileMenu from "../scripts/modules/MobileMenu";
 import StickyHeader from "../scripts/modules/StickyHeader";
 import RevealOnScroll from "../scripts/modules/RevealOnScroll";
+import Modal from "../scripts/modules/Modal";
 // accept the hot module updates
 if (module.hot) {
   module.hot.accept();
@@ -9,6 +10,7 @@ if (module.hot) {
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
+new Modal();
 
 // create a reusable class
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
